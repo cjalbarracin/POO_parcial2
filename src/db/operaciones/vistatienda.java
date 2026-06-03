824eef0eca6138e603885fb6dc27681f3b8e099a
usuario <cjalbarracin@udistrital.edu.co>
2026-06-03 13:32:06 -0500

    INTERFAZ TERMINADA

---
+++ src/db/operaciones/vistatienda.java
@@ -0,0 +1,25 @@
+package db.operaciones;
+
+import javax.swing.*;
+
+public class vistatienda {
+    private JPanel panel1;
+    private JTextField txtPrecio;
+    private JTextField txtAlmacenamiento;
+    private JTextField txtCamara;
+    private JTextField txtModelo;
+    private JTextField txtMarca;
+    private JTextField txtRAM;
+    private JTextField txtBateria;
+    private JButton btnRegistrar;
+    private JComboBox cmbBuscarPor;
+    private JTextField txtBuscar;
+    private JButton btnAplicarFiltro;
+    private JTextArea txtAreaResultados;
+    private JButton btnConsultarTodo;
+    private JButton btnDescargarReporte;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
